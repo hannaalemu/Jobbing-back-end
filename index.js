73e809236f08a8f46983b915f98313233eaca69a
hannaalemu <hanna9alemu@outlook.com>
2019-09-30 16:12:29 -0700

    added directory structure, server up and running, added proof of life tests, eslint passing, google docs route working

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const App = require('./src/app');
+
+require('dotenv').config();
+
+const MONGOOSE_URI = 'mongodb+srv://hanna9:estifaman9@cluster0-s90so.mongodb.net/test?retryWrites=true&w=majority';
+
+// Hanna - connect to database
+mongoose.connect(MONGOOSE_URI, { useNewUrlParser: true });
+
+// Hanna -host app on port 3000
+App.start(process.env.PORT || 3000);
