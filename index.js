4d8442205f5e6fbb41219f76fb83e18f7d1ff132
hannaalemu <hanna9alemu@outlook.com>
2019-10-05 20:39:31 -0700

    updated application to use state instead of redux, and use bootstrap instead

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const App = require('./src/app');
 
 require('dotenv').config();
 
-const MONGOOSE_URI = 'mongodb+srv://hanna9:estifaman9@cluster0-s90so.mongodb.net/test?retryWrites=true&w=majority';
+const MONGOOSE_URI = 'mongodb://hanna9:estifaman9@cluster0-shard-00-00-s90so.mongodb.net:27017,cluster0-shard-00-01-s90so.mongodb.net:27017,cluster0-shard-00-02-s90so.mongodb.net:27017/jobbing?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 // Hanna - connect to database
 mongoose.connect(MONGOOSE_URI, { 
